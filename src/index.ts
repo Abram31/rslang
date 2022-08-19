77c0d6164eaa380fcb8a342df4910cb875569814
Veronika2811 <smeyun@list.ru>
2022-08-19 17:50:37 +0300

    feat: add page text book

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import './global.scss';
+import ChooseSectionTextbook from './modules/layouts/textbookLevel/TextbookLevel';
 import FooterRender from './modules/layouts/footer/FooterRender';
 import HeaderRender from './modules/layouts/header/HeaderRender';
-import MiniGamesListRender from './modules/layouts/miniGames/MiniGamesListRender';
 
 new HeaderRender();
-new MiniGamesListRender();
+new ChooseSectionTextbook();
 new FooterRender();
